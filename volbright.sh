3d453a1cf46fad8158ba848ec307601cc78f05f8
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2024-01-26 14:10:59 +0400

    Add files via upload

--- volbright.sh
+++ volbright.sh
@@ -2,6 +2,6 @@ case "$1" in
 	"--upvol" ) pulsemixer --change-volume +1 && pkill -SIGUSR1 -f dwmbar.sh;;
 	"--downvol" ) pulsemixer --change-volume -1 && pkill -SIGUSR1 -f dwmbar.sh;;
 	"--mutevol" ) pulsemixer --toggle-mute && pkill -SIGUSR1 -f dwmbar.sh;;
-	"--uplight" ) light -A 1 && pkill -SIGUSR2 -f dwmbar.sh;;
-	"--downlight" ) light -U 1  && pkill -SIGUSR2 -f dwmbar.sh;;
+	"--uplight" ) brightnessctl s 1%+ && pkill -SIGUSR2 -f dwmbar.sh;;
+	"--downlight" ) brightnessctl s 1%-  && pkill -SIGUSR2 -f dwmbar.sh;;
 esac
