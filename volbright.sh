28f506e394af811101e5670db992612151eda52b
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2024-01-30 19:39:11 +0400

    Delete volbright.sh

--- volbright.sh
+++
@@ -1,7 +0,0 @@
-case "$1" in
-	"--upvol" ) pulsemixer --change-volume +1 && pkill -SIGUSR1 -f dwmbar.sh;;
-	"--downvol" ) pulsemixer --change-volume -1 && pkill -SIGUSR1 -f dwmbar.sh;;
-	"--mutevol" ) pulsemixer --toggle-mute && pkill -SIGUSR1 -f dwmbar.sh;;
-	"--uplight" ) brightnessctl s 1%+ && pkill -SIGUSR2 -f dwmbar.sh;;
-	"--downlight" ) brightnessctl s 1%-  && pkill -SIGUSR2 -f dwmbar.sh;;
-esac
