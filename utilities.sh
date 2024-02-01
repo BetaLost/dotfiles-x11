9e0f514f2c122cd02319fa23d46567271b61cecd
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2024-02-01 21:59:44 +0400

    Update utilities.sh

--- utilities.sh
+++ utilities.sh
@@ -5,6 +5,6 @@ case "$1" in
 	"--uplight" ) brightnessctl s 1%+ && pkill -SIGUSR2 -f dwmbar.sh;;
 	"--downlight" ) brightnessctl s 1%-  && pkill -SIGUSR2 -f dwmbar.sh;;
 	"--weather" ) st -c floatst -g 45x8 -e sh -c "curl wttr.in/?0; read";; 
-	"--clock" ) st -c floatst -g 56x9 -e sh -c "tty-clock -s -C 1";;
+	"--clock" ) st -c floatst -g 56x9 -e sh -c "tty-clock -s -c -C 1";;
 	"--neofetch" ) st -c floatst -g 66x16 -e sh -c "neofetch; read";;
 esac
